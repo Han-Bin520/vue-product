56bd2f95d14bc87247af6bee02b68c5c2f77e1c3
Han-Bin520 <1263256614@qq.com>
2020-09-17 18:24:14 +0800

    头部模式和导航路由

--- music/src/App.vue
+++ music/src/App.vue
@@ -1,14 +1,40 @@
 <template>
   <div id="app">
-    <div class="test"></div>
-    <div>hello world</div>
+    <Header></Header>
+    <Tabbar></Tabbar>
+    <router-view></router-view>
   </div>
 </template>
 
+<script>
+import Header from './components/Header'
+import Tabbar from './components/Tabbar'
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Tabbar
+  }
+}
+</script>
 <style lang="scss">
-  .test{
+  @import "assets/css/variable";
+  .theme{
+    position: fixed;
+    left: 0;
+    bottom: 0;
+  }
+  button{
     width: 100px;
     height: 100px;
-    background: #f00;
+    &:nth-of-type(1){
+      background: $background-color-theme;
+    }
+    &:nth-of-type(2){
+      background: $background-color-theme1;
+    }
+    &:nth-of-type(3){
+      background: $background-color-theme2;
+    }
   }
 </style>
--- music/src/router/index.js
+++ music/src/router/index.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Recommend from '../views/Recommend'
+import Rank from '../views/Rank'
+import Search from '../views/Search'
+import Singer from '../views/Singer'
 
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/recommend', component: Recommend },
+  { path: '/singer', component: Singer },
+  { path: '/rank', component: Rank },
+  { path: '/search', component: Search }
 ]
 
 const router = new VueRouter({
