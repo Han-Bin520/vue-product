5b1d083de5ff8b3c75898840077b0cf99a229c1e
Han-Bin520 <1263256614@qq.com>
2020-09-17 17:02:00 +0800

    初始化项目

--- music/src/App.vue
+++ music/src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app">
-
+    <div class="test"></div>
+    <div>hello world</div>
   </div>
 </template>
 
 <style lang="scss">
-
-
+  .test{
+    width: 100px;
+    height: 100px;
+    background: #f00;
+  }
 </style>
