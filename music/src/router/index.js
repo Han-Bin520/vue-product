27ddac169c027f856f9c2425b5b213cf669cfd6e
Han-Bin520 <1263256614@qq.com>
2020-09-19 17:53:02 +0800

    Axios网络请求模块封装和banner封装

--- music/src/router/index.js
+++ music/src/router/index.js
@@ -1,10 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Recommend from '../views/Recommend'
-import Rank from '../views/Rank'
-import Search from '../views/Search'
-import Singer from '../views/Singer'
+// import Recommend from '../views/Recommend'
+// import Rank from '../views/Rank'
+// import Search from '../views/Search'
+// import Singer from '../views/Singer'
 
+// Vue组件按需加载
+const Recommend = (resolve) => {
+  import('../views/Recommend').then((module) => {
+    resolve(module)
+  })
+}
+const Rank = (resolve) => {
+  import('../views/Rank').then((module) => {
+    resolve(module)
+  })
+}
+const Search = (resolve) => {
+  import('../views/Search').then((module) => {
+    resolve(module)
+  })
+}
+const Singer = (resolve) => {
+  import('../views/Singer').then((module) => {
+    resolve(module)
+  })
+}
 Vue.use(VueRouter)
 
 const routes = [
--- music/src/views/Recommend.vue
+++ music/src/views/Recommend.vue
@@ -1,10 +1,43 @@
 <template>
-    <div>首页</div>
+    <div class="recommend">
+        <Banner :banners="banners"></Banner>
+        <Personalized></Personalized>
+    </div>
 </template>
 
 <script>
+import { getBanner, getPersonalized } from '../api/index'
+import Banner from '../components/Banner'
+import Personalized from '../components/Personalized'
+
 export default {
-  name: 'Recommend'
+  name: 'Recommend',
+  components: {
+    Banner,
+    Personalized
+  },
+  data () {
+    return {
+      banners: []
+    }
+  },
+  created () {
+    getBanner()
+      .then((data) => {
+        this.banners = data.banners
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
+    getPersonalized()
+      .then((data) => {
+        this.personalized = data.result
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
+  }
+
 }
 </script>
 
